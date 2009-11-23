dda98a9ae146c672f315230a5833cd4157402c98
André Rømcke <ar@ez.no>
2009-11-23 15:34:31 +0000

    - Fixed bug #15861: Several bugs in websitetoolbar/sort   (Merged from trunk rev. 20)
    git-svn-id: http://svn.projects.ez.no/ezwebsitetoolbar/stable/1.1@21 036ce2ec-5c54-4936-9182-039a592c728d

--- packages/ezwt_extension/ezextension/ezwt/design/standard/javascript/ezwtsortdd.js
+++ packages/ezwt_extension/ezextension/ezwt/design/standard/javascript/ezwtsortdd.js
@@ -1,42 +1,70 @@
 var eZWTSortDD = function() {
     var ret = {};
     
+    ret.enabled = false;
     ret.CONFIG = {};
     
-    var yCallback = function(Y) {
-        Y.get('#ezwt-sort-field').on('change', function(e) {
-            var select = e.target;
-            var selectedIndex = select.get('selectedIndex');
+    var setupSortChangeEvent = function( Y )
+    {
+        Y.get('#ezwt-sort-field').on('change', function( e )
+        {
+            var select = e.target, selectedIndex = select.get('selectedIndex');
 
-            if(select.get('options').item(selectedIndex).get('value') == 8) {
+            if( select.get('options').item(selectedIndex).get('value') == 8 )
+            {
                 Y.get('#ezwt-sort-order').set('disabled', true);
-                
-                if(Y.get('#ezwt-sort-order-asc') === null) {
-                    select.get('parentNode').appendChild(Y.Node.create('<input id="ezwt-sort-order-asc" type="hidden" name="SortingOrder" value="1" />'));
+
+                if ( ret.enabled )
+                {
+                    Y.get('#ezwt-automatic-update-container').removeClass('hide');
+                    if ( !Y.get('#ezwt-automatic-update').get('checked') )
+                        Y.get('#ezwt-update-priority').set('disabled', false).replaceClass('button-disabled', 'button');
                 }
-            } else {
+
+                if( Y.get('#ezwt-sort-order-asc') === null )
+                {
+                    select.get('parentNode').appendChild( Y.Node.create('<input id="ezwt-sort-order-asc" type="hidden" name="SortingOrder" value="1" />') );
+                }
+            }
+            else
+            {
                 Y.get('#ezwt-sort-order').set('disabled', false);
-                
-                if(Y.get('#ezwt-sort-order-asc') !== null) {
+
+                if ( ret.enabled )
+                {
+                    Y.get('#ezwt-automatic-update-container').addClass('hide');
+                    Y.get('#ezwt-update-priority').set('disabled', true).replaceClass('button', 'button-disabled');
+                }
+
+                if(Y.get('#ezwt-sort-order-asc') !== null)
+                {
                     select.get('parentNode').removeChild(Y.get('#ezwt-sort-order-asc'));
                 }
             }
         });
-        
-        Y.get('#ezwt-automatic-update').on('click', function(e) {
-            if(e.target.get('checked')) {
+    };
+    
+    var yCallback = function(Y)
+    {
+        setupSortChangeEvent( Y );
+
+        Y.get('#ezwt-automatic-update').on('click', function(e)
+        {
+            if(e.target.get('checked'))
                 Y.get('#ezwt-update-priority').set('disabled', true).replaceClass('button', 'button-disabled');
-            } else {
+            else
                 Y.get('#ezwt-update-priority').set('disabled', false).replaceClass('button-disabled', 'button');
-            }
         });
         
-        var ioCallback = function(id, o) {
-            if (o.responseText !== undefined) {
+        var ioCallback = function(id, o)
+        {
+            if (o.responseText !== undefined)
+            {
             }
         }
         
-        Y.DD.DDM.on('drop:over', function(e) {
+        Y.DD.DDM.on('drop:over', function(e)
+        {
             var drag = e.drag.get('node'),
                 drop = e.drop.get('node');
 
@@ -50,18 +78,23 @@ var eZWTSortDD = function() {
             }
         });
 
-        Y.DD.DDM.on('drag:drag', function(e) {
+        Y.DD.DDM.on('drag:drag', function(e)
+        {
             var y = e.target.lastXY[1];
 
-            if (y < lastY) {
+            if (y < lastY)
+            {
                 goingUp = true;
-            } else {
+            }
+            else
+            {
                 goingUp = false;
             }
             lastY = y;
         });
 
-        Y.DD.DDM.on('drag:start', function(e) {
+        Y.DD.DDM.on('drag:start', function(e)
+        {
             var drag = e.target;
 
             drag.get('node').setStyle('opacity', '.25');
@@ -73,7 +106,8 @@ var eZWTSortDD = function() {
             });
         });
 
-        Y.DD.DDM.on('drag:end', function(e) {
+        Y.DD.DDM.on('drag:end', function(e)
+        {
             var drag = e.target;
 
             drag.get('node').setStyles({
@@ -82,51 +116,64 @@ var eZWTSortDD = function() {
             });
             
             var priority = 0;
-            Y.all('#ezwt-sort-list input.ezwt-priority-input').each(function(i, v) {
+            Y.all('#ezwt-sort-list input.ezwt-priority-input').each(function(i, v)
+            {
                 i.set('value', priority);
                 priority += 10;
             });
             
-            if(Y.get('#ezwt-automatic-update').get('checked')) {
+            if( Y.get('#ezwt-automatic-update').get('checked') )
+            {
                 Y.io.ez('ezwt::updatepriority', {on: {success: ioCallback}, form: { id: 'ezwt-sort-form', upload: false }, method: 'POST'});
             }
         });
 
-        Y.DD.DDM.on('drag:drophit', function(e) {
-            var drop = e.drop.get('node'),
-                drag = e.drag.get('node');
+        Y.DD.DDM.on('drag:drophit', function(e)
+        {
+            var drop = e.drop.get('node'), drag = e.drag.get('node');
 
-            if (drop.get('tagName').toLowerCase() !== 'tr') {
-                if (!drop.contains(drag)) {
-                    drop.appendChild(drag);
+            if ( drop.get('tagName').toLowerCase() !== 'tr' )
+            {
+                if ( !drop.contains(drag) )
+                {
+                    drop.appendChild( drag );
                 }
             }
         });
 
         var goingUp = false, lastY = 0;
-
-        var dragList = Y.Node.all('.ezwt-sort-drag-handler');
-        dragList.each(function(v, k) {
+        var dragdropList = Y.Node.all('tr.ezwt-sort-dragable').addClass('ezwt-sort-drag-handler');
+        dragdropList.each(function(v, k)
+        {
             var dd = new Y.DD.Drag({
                 node: v
-            }).plug(Y.Plugin.DDProxy, {
+            }).plug( Y.Plugin.DDProxy, {
                 moveOnEnd: false
-            }).plug(Y.Plugin.DDConstrained, {
+            }).plug( Y.Plugin.DDConstrained, {
                 constrain2node: '#ezwt-sort-list'
             });
         });
 
-        var dropList = Y.Node.all('.ezwt-sort-drag-handler');
-        dropList.each(function(v, k) {
+        dragdropList.each(function(v, k)
+        {
             var tar = new Y.DD.Drop({
                 node: v
             });
         });
     }
     
-    ret.init = function() {
-        var Y = YUI(YUI3_config).use('node', 'io-ez', 'io-form', 'dd-constrain', 'dd-proxy', 'dd-drop', yCallback);
+    ret.init = function()
+    {
+        if ( document.getElementById('ezwt-sort-field').value == 8 )
+        {
+            YUI(YUI3_config).use('node', 'io-ez', 'io-form', 'dd-constrain', 'dd-proxy', 'dd-drop', yCallback );
+            ret.enabled = true;
+        }
+        else
+        {
+            YUI(YUI3_config).use('node', setupSortChangeEvent );
+        }
     }
-    
+
     return ret;
 }();
