6d7772b9a6b953b6ec498dddd9e035ef2bc7b6e4
Nicolas Pastorino <nfrp@ez.no>
2010-03-10 11:56:10 +0000

    --Brazilian translation updated, credits go to Renan Leme
    git-svn-id: http://svn.projects.ez.no/ezwebsitetoolbar/trunk@30 036ce2ec-5c54-4936-9182-039a592c728d

--- packages/ezwt_extension/ezextension/ezwt/translations/por-BR/translation.ts
+++ packages/ezwt_extension/ezextension/ezwt/translations/por-BR/translation.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE TS>
-<TS>
+<!DOCTYPE TS><TS>
 <context>
     <name>design/standard/content/edit</name>
     <message>
@@ -9,11 +7,11 @@
     </message>
     <message>
         <source>Manage versions</source>
-        <translation>Administrar versões</translation>
+        <translation>Gerenciar versões</translation>
     </message>
     <message>
         <source>Store and exit</source>
-        <translation>Armazenar e sair</translation>
+        <translation>Salvar e sair</translation>
     </message>
     <message>
         <source>Preview</source>
@@ -21,7 +19,7 @@
     </message>
     <message>
         <source>Translate from</source>
-        <translation type="unfinished"></translation>
+        <translation>Traduzir a partir de</translation>
     </message>
     <message>
         <source>Translate</source>
@@ -32,7 +30,7 @@
     <name>design/standard/content/view/versionview</name>
     <message>
         <source>Manage versions</source>
-        <translation>Administrar versões</translation>
+        <translation>Gerenciar versões</translation>
     </message>
     <message>
         <source>Edit</source>
@@ -51,7 +49,7 @@
     </message>
     <message>
         <source>Edit: %node_name [%class_name]</source>
-        <translation type="unfinished"></translation>
+        <translation>Editar: %node_name [%class_name]</translation>
     </message>
     <message>
         <source>Move</source>
@@ -63,11 +61,11 @@
     </message>
     <message>
         <source>Add locations</source>
-        <translation type="unfinished"></translation>
+        <translation>Adicionar locais</translation>
     </message>
     <message>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Classificação</translation>
     </message>
     <message>
         <source>Replace</source>
@@ -83,38 +81,42 @@
     </message>
     <message>
         <source>Documentation</source>
-        <translation type="unfinished"></translation>
+        <translation>Documentação</translation>
     </message>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Sobre</translation>
     </message>
     <message>
         <source>Edit object states</source>
-        <translation type="unfinished"></translation>
+        <translation>Editar estados do objeto</translation>
+    </message>
+    <message>
+        <source>Toggle menu link type edit.</source>
+        <translation>Alternar tipo de link.</translation>
     </message>
 </context>
 <context>
     <name>design/standard/websitetoolbar/sort</name>
     <message>
         <source>Sub items [%children_count]</source>
-        <translation type="unfinished"></translation>
+        <translation>Subitens [%children_count]</translation>
     </message>
     <message>
         <source>Show 10 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Exibir 10 itens por página.</translation>
     </message>
     <message>
         <source>Show 50 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Exibir 50 itens por página.</translation>
     </message>
     <message>
         <source>Show 25 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Exibir 25 itens por página.</translation>
     </message>
     <message>
         <source>Priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Prioridade</translation>
     </message>
     <message>
         <source>Name</source>
@@ -126,7 +128,7 @@
     </message>
     <message>
         <source>Modifier</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">Modificador</translation>
     </message>
     <message>
         <source>Modified</source>
@@ -137,48 +139,48 @@
         <translation>Seção</translation>
     </message>
     <message>
-        <source>Use the priority fields to control the order in which the items appear. You can use both positive and negative integers. Click the "Update priorities" button to apply the changes.</source>
-        <translation type="unfinished"></translation>
+        <source>Use the priority fields to control the order in which the items appear. You can use both positive and negative integers. Click the &quot;Update priorities&quot; button to apply the changes.</source>
+        <translation>Use os campos de prioridade para controlar a ordem em que os itens aparecem. Você pode usar tanto números positivos como negativos. Clique no botão &quot;Atualizar as prioridades&quot; para aplicar as alterações.</translation>
     </message>
     <message>
         <source>You are not allowed to update the priorities because you do not have permission to edit &lt;%node_name&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Você não pode alterar as prioridades porque você não tem permissão para editar o item &lt;%node_name&gt;.</translation>
     </message>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">Desconhecido</translation>
     </message>
     <message>
         <source>The current item does not contain any sub items.</source>
-        <translation type="unfinished"></translation>
+        <translation>O item atual não possui nenhum subitem.</translation>
     </message>
     <message>
         <source>Update priorities</source>
-        <translation type="unfinished"></translation>
+        <translation>Atualizar as prioridades</translation>
     </message>
     <message>
         <source>Apply changes to the priorities of the items in the list above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplicar as modificações às prioridades dos itens da lista acima.</translation>
     </message>
     <message>
         <source>You cannot update the priorities because you do not have permission to edit the current item or because a non-priority sorting method is used.</source>
-        <translation type="unfinished"></translation>
+        <translation>Você não pode atualizar as prioridades porque você não tem permissão para editar o item atual ou porque um método de classificação por não-prioridade está sendo utilizado.</translation>
     </message>
     <message>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Classificação</translation>
     </message>
     <message>
         <source>Class identifier</source>
-        <translation type="unfinished"></translation>
+        <translation>Indentificador da classe</translation>
     </message>
     <message>
         <source>Class name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nome da classe</translation>
     </message>
     <message>
         <source>Depth</source>
-        <translation type="unfinished"></translation>
+        <translation>Profundidade</translation>
     </message>
     <message>
         <source>Published</source>
@@ -186,27 +188,47 @@
     </message>
     <message>
         <source>You cannot set the sorting method for the current location because you do not have permission to edit the current item.</source>
-        <translation type="unfinished"></translation>
+        <translation>Você não pode definir o método de classificação para o local atual, pois você não tem permissão para editar o item atual.</translation>
     </message>
     <message>
         <source>Use these controls to set the sorting method for the sub items of the current location.</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilize estes controles para definir o método de classificação para os subitens do local atual.</translation>
     </message>
     <message>
         <source>Descending</source>
-        <translation type="unfinished"></translation>
+        <translation>Decrescente</translation>
     </message>
     <message>
         <source>Ascending</source>
-        <translation type="unfinished"></translation>
+        <translation>Crescente</translation>
     </message>
     <message>
         <source>Set</source>
-        <translation type="unfinished"></translation>
+        <translation>Definir</translation>
     </message>
     <message>
         <source>Invalid or missing parameter: %parameter</source>
-        <translation type="unfinished"></translation>
+        <translation>O parâmetro está faltando ou é inválido: %parameter</translation>
+    </message>
+    <message>
+        <source>Use these checkboxes to select items for removal. Click the &quot;Remove selected&quot; button to  remove the selected items.</source>
+        <translation>Use estas opções para selecionar os itens a serem removidos. Clique no botão &quot;Remover selecionados&quot; para remover os itens selecionados.</translation>
+    </message>
+    <message>
+        <source>You do not have permission to remove this item.</source>
+        <translation>Você não tem permissão para remover este item.</translation>
+    </message>
+    <message>
+        <source>Remove the selected items from the list above.</source>
+        <translation>Remover os itens selecionados da lista acima.</translation>
+    </message>
+    <message>
+        <source>You do not have permission to remove any of the items from the list above.</source>
+        <translation>Você não tem permissão para remover qualquer um dos itens da lista acima.</translation>
+    </message>
+    <message>
+        <source>Automaitc update</source>
+        <translation>Atualização automática</translation>
     </message>
 </context>
 </TS>
