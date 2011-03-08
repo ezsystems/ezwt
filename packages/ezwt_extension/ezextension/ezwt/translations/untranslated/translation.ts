1f566dcb6671ed17182200f3eefca55a2b85f518
Damien Pobel <damien.pobel__AT__smile.fr>
2011-03-08 17:00:18 +0800

    Localization: updated base translations.ts file

--- packages/ezwt_extension/ezextension/ezwt/translations/untranslated/translation.ts
+++ packages/ezwt_extension/ezextension/ezwt/translations/untranslated/translation.ts
@@ -1,4 +1,13 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>design/admin/node/view/full</name>
+    <message>
+        <source>Remove selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 <context>
     <name>design/standard/content/edit</name>
     <message>
@@ -7,10 +16,6 @@
     </message>
     <message>
         <source>Manage versions</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Store and exit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -25,13 +30,41 @@
         <source>Translate</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Publish the contents of the draft that is being edited. The draft will become the published version of the object.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Store draft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Store the contents of the draft that is being edited and continue editing. Use this button to periodically save your work while editing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Store draft and exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Store the draft that is being edited and exit from edit mode. Use when you need to exit your work and return later to continue.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Discard draft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure you want to discard the draft?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Discard the draft that is being edited. This will also remove the translations that belong to the draft (if any).</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>design/standard/content/view/versionview</name>
-    <message>
-        <source>Manage versions</source>
-        <translation type="unfinished"></translation>
-    </message>
     <message>
         <source>Edit</source>
         <translation type="unfinished"></translation>
@@ -45,10 +78,6 @@
     <name>design/standard/parts/website_toolbar</name>
     <message>
         <source>Create here</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Edit: %node_name [%class_name]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -91,6 +120,18 @@
         <source>Edit object states</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Go to admin interface.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Toggle menu link type edit.</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>design/standard/websitetoolbar/sort</name>
@@ -123,10 +164,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Modifier</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Modified</source>
         <translation type="unfinished"></translation>
     </message>
@@ -140,10 +177,6 @@
     </message>
     <message>
         <source>You are not allowed to update the priorities because you do not have permission to edit &lt;%node_name&gt;.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Unknown</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -204,6 +237,30 @@
     </message>
     <message>
         <source>Invalid or missing parameter: %parameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use these checkboxes to select items for removal. Click the &quot;Remove selected&quot; button to  remove the selected items.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You do not have permission to remove this item.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove the selected items from the list above.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You do not have permission to remove any of the items from the list above.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Automatic update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path String</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
