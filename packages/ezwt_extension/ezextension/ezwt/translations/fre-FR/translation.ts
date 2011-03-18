6b9ef2a87770dbe4e586ab7826377f6149d134e2
Damien Pobel <dpobel@free.fr>
2011-03-18 23:07:41 +0800

    Localization fre-FR: added missing translations

--- packages/ezwt_extension/ezextension/ezwt/translations/fre-FR/translation.ts
+++ packages/ezwt_extension/ezextension/ezwt/translations/fre-FR/translation.ts
@@ -5,7 +5,7 @@
     <name>design/admin/node/view/full</name>
     <message>
         <source>Remove selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer la sélection</translation>
     </message>
 </context>
 <context>
@@ -36,35 +36,35 @@
     </message>
     <message>
         <source>Publish the contents of the draft that is being edited. The draft will become the published version of the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Publier le contenu du brouillon en court de modification. Ce brouillon sera la version publiée de l&apos;objet.</translation>
     </message>
     <message>
         <source>Store draft</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregistrer le brouillon</translation>
     </message>
     <message>
         <source>Store the contents of the draft that is being edited and continue editing. Use this button to periodically save your work while editing.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sauvegarder le contenu du brouillon qui est actuellement modifié et continuer la modification. Utilisez ce bouton périodiquement pour sauvegarder votre travail tout en le modifiant.</translation>
     </message>
     <message>
         <source>Store draft and exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregistrer le brouillon et quitter</translation>
     </message>
     <message>
         <source>Store the draft that is being edited and exit from edit mode. Use when you need to exit your work and return later to continue.</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregistrer le brouillon en cours de modification et quitter le mode modification. Utilisez ce bouton pour enregistrer votre travail pour revenir plus tard.</translation>
     </message>
     <message>
         <source>Discard draft</source>
-        <translation type="unfinished"></translation>
+        <translation>Annuler le brouillon</translation>
     </message>
     <message>
         <source>Are you sure you want to discard the draft?</source>
-        <translation type="unfinished"></translation>
+        <translation>Voulez-vous vraiment supprimer le brouillon ?</translation>
     </message>
     <message>
         <source>Discard the draft that is being edited. This will also remove the translations that belong to the draft (if any).</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer le brouillon actuellement modifié. Cette action supprimera aussi les traductions qui appartiennent au brouillon (si elles existent).</translation>
     </message>
 </context>
 <context>
@@ -75,7 +75,7 @@
     </message>
     <message>
         <source>Edit</source>
-        <translation>Editer</translation>
+        <translation>Modifier</translation>
     </message>
     <message>
         <source>Publish</source>
@@ -106,7 +106,7 @@
     </message>
     <message>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Tri</translation>
     </message>
     <message>
         <source>Replace</source>
@@ -126,46 +126,46 @@
     </message>
     <message>
         <source>About</source>
-        <translation>Au sujet de</translation>
+        <translation>À propos</translation>
     </message>
     <message>
         <source>Edit object states</source>
-        <translation type="unfinished"></translation>
+        <translation>Modifier les états de l'objet</translation>
     </message>
     <message>
         <source>Edit</source>
-        <translation type="unfinished">Editer</translation>
+        <translation>Modifier</translation>
     </message>
     <message>
         <source>Go to admin interface.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aller dans l'interface d'administration</translation>
     </message>
     <message>
         <source>Toggle menu link type edit.</source>
-        <translation type="unfinished"></translation>
+        <translation>(Dés)activer le mode modification du menu.</translation>
     </message>
 </context>
 <context>
     <name>design/standard/websitetoolbar/sort</name>
     <message>
         <source>Sub items [%children_count]</source>
-        <translation type="unfinished"></translation>
+        <translation>Sous-éléments [%children_count]</translation>
     </message>
     <message>
         <source>Show 10 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher 10 éléments par page.</translation>
     </message>
     <message>
         <source>Show 50 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher 50 éléments par page.</translation>
     </message>
     <message>
         <source>Show 25 items per page.</source>
-        <translation type="unfinished"></translation>
+        <translation>Afficher 25 éléments par page.</translation>
     </message>
     <message>
         <source>Priority</source>
-        <translation type="unfinished"></translation>
+        <translation>Priorité</translation>
     </message>
     <message>
         <source>Name</source>
@@ -185,11 +185,11 @@
     </message>
     <message>
         <source>Use the priority fields to control the order in which the items appear. You can use both positive and negative integers. Click the &quot;Update priorities&quot; button to apply the changes.</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilisez les champs priorité pour contrôler l&apos;ordre dans lequel les éléments apparaissent. Vous pouvez utiliser des priorités positives et négatives. Cliquez sur le bouton &quot;Mettre à jour les priorités&quot; pour enregistrer les changements.</translation>
     </message>
     <message>
         <source>You are not allowed to update the priorities because you do not have permission to edit &lt;%node_name&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vous n&apos;êtes pas autorisé à mettre à jour les priorités parce que vous n&apos;avez pas le droit de modifier &lt;%node_name&gt;.</translation>
     </message>
     <message>
         <source>Unknown</source>
@@ -197,35 +197,35 @@
     </message>
     <message>
         <source>The current item does not contain any sub items.</source>
-        <translation type="unfinished"></translation>
+        <translation>L&apos;élément actuel ne contient aucun sous-élément.</translation>
     </message>
     <message>
         <source>Update priorities</source>
-        <translation type="unfinished"></translation>
+        <translation>Mettre à jour les priorités</translation>
     </message>
     <message>
         <source>Apply changes to the priorities of the items in the list above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Appliquer les changements aux priorités des éléments de la liste ci-dessus.</translation>
     </message>
     <message>
         <source>You cannot update the priorities because you do not have permission to edit the current item or because a non-priority sorting method is used.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vous ne pouvez pas mettre à jour les priorités parce que vous n&apos;avez pas le droit de modifier l&apos;élément sélectionné ou parce que la priorité n&apos;est pas utilisé comme critère de tri.</translation>
     </message>
     <message>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Tri</translation>
     </message>
     <message>
         <source>Class identifier</source>
-        <translation type="unfinished"></translation>
+        <translation>Identifiant de classe</translation>
     </message>
     <message>
         <source>Class name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nom de classe</translation>
     </message>
     <message>
         <source>Depth</source>
-        <translation type="unfinished"></translation>
+        <translation>Profondeur</translation>
     </message>
     <message>
         <source>Published</source>
@@ -233,11 +233,11 @@
     </message>
     <message>
         <source>You cannot set the sorting method for the current location because you do not have permission to edit the current item.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vous ne pouvez pas définir la méthode de tri à l&apos;emplacement actuel parce que vous n&apos;avez pas le droit de modifier l&apos;élément courant.</translation>
     </message>
     <message>
         <source>Use these controls to set the sorting method for the sub items of the current location.</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilisez ces contrôles pour modifier la méthode de tri des sous-éléments de l&apos;emplacement actuel.</translation>
     </message>
     <message>
         <source>Descending</source>
@@ -249,31 +249,31 @@
     </message>
     <message>
         <source>Set</source>
-        <translation type="unfinished"></translation>
+        <translation>Affecter</translation>
     </message>
     <message>
         <source>Invalid or missing parameter: %parameter</source>
-        <translation type="unfinished"></translation>
+        <translation>Paramètre manquant ou invalide : %parameter</translation>
     </message>
     <message>
         <source>Use these checkboxes to select items for removal. Click the &quot;Remove selected&quot; button to  remove the selected items.</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilisez ces cases à cocher pour sélectionner les éléments à supprimer. Cliquez sur le bouton &quot;Supprimer la sélection&quot; pour supprimer les éléments sélectionnés.</translation>
     </message>
     <message>
         <source>You do not have permission to remove this item.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vous n&apos;avez pas le droit de supprimer cet élément.</translation>
     </message>
     <message>
         <source>Remove the selected items from the list above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Supprimer les éléments sélectionnés dans la liste ci-dessus.</translation>
     </message>
     <message>
         <source>You do not have permission to remove any of the items from the list above.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vous n&apos;avez le droit de supprimer aucun des éléments de la liste ci-dessus.</translation>
     </message>
     <message>
         <source>Automatic update</source>
-        <translation type="unfinished"></translation>
+        <translation>Mise à jour automatique</translation>
     </message>
 </context>
 </TS>
