33337f1cb83a9843630c12eaf561c0ac496713cc
Bertrand Dunogier <bd@ez.no>
2011-03-29 17:16:28 +0200

    Fixed license in ezinfo.php

--- packages/ezwt_extension/ezextension/ezwt/ezinfo.php
+++ packages/ezwt_extension/ezextension/ezwt/ezinfo.php
@@ -31,7 +31,7 @@ class ezwtInfo
         return array( 'Name' => 'eZ Website Toolbar',
                       'Version' => '//autogentag//',
                       'Copyright' => 'Copyright (C) 1999-2010 eZ Systems AS',
-                      'License' => '//EZP_LICENSE//'
+                      'License' => 'GNU General Public License v2.0'
                      );
     }
 }
